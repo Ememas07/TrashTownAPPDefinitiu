6603e24dee0c7f0fc15b4eed03e03d1b16c593f1
Marc Mas <marcmashernandez@gmail.com>
2024-02-09 13:13:33 +0100

    dues pagines funcionals!!

--- app/src/main/java/com/example/trashtownapp6/MainActivity.kt
+++ app/src/main/java/com/example/trashtownapp6/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.diy_fragment, R.id.navigation_diy
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_diy, R.id.navigation_ajustes
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
